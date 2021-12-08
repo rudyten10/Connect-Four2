d4be9168b1eae2f0933baec65503a800c896befd
rudyten10 <94325541+rudyten10@users.noreply.github.com>
2021-12-07 19:28:16 -0800

    connect-four stage 1/5
    stage 1/5

---
+++ Main.kt
@@ -0,0 +1,59 @@
+package connectfour
+
+fun main() {
+    var rows = 0
+    var cols = 0
+
+    val validRegex = Regex("([0-9][0-9]?)x([0-9][0-9]?)")
+    val validRangeRegex = Regex("([5-9])x([5-9])")
+
+    println("Connect Four")
+    println("First player's name:")
+    val fPlayerName = readLine()!!
+    println("Second player's name:")
+    val sPlayerName = readLine()!!
+
+    // loop@ while (true) {
+    while (true) {
+        //println("+++++++++++")
+        println("Set the board dimensions (Rows x Columns)")
+        println("Press Enter for default (6 x 7)")
+        //val input = "5 X 6".trim().replace(" ", "").lowercase()
+        //var input = readLine()!!.trim().replace(" ", "").lowercase()
+        //var input = readLine()!!.trim().replace("\\s+".toRegex(), "").lowercase()
+        var input = readLine()!!.trim().replace("\\s+".toRegex(), "").lowercase()
+
+        if (input == "") {
+            rows = 6
+            cols = 7
+            break
+        }
+
+        if (input.matches(validRegex)) {
+            val tmp = input.split("x")
+            rows = tmp[0].toString().toInt()
+            cols = tmp[1].toString().toInt()
+            if (input.matches(validRangeRegex)) {
+                break
+            } else {
+                if (rows < 5 ||rows > 9) {
+                    println("Board rows should be from 5 to 9")
+                } else if (cols < 5 || cols > 9) {
+                    println("Board columns should be from 5 to 9")
+                }
+                //continue@loop
+            }
+        } else {
+            //println("2222+++++++++++")
+            println("Invalid input")
+            //continue@loop
+        }
+    }
+
+    println("$fPlayerName VS $sPlayerName")
+    println("$rows X $cols board")
+
+
+
+
+}
